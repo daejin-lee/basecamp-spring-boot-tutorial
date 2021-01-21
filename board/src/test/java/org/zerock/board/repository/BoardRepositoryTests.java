ba236b512f7c8872f8229afe12f170aeb4c11eb3
daejin-lee <daejin.lee@nhn.com>
2021-01-21 12:02:50 +0900

    Update BoardRepositoryTests.java

--- board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
+++ board/src/test/java/org/zerock/board/repository/BoardRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
+import javax.transaction.Transactional;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -27,6 +28,7 @@ public class BoardRepositoryTests {
         });
     }
 
+    @Transactional
     @Test
     public void testRead1(){
         Optional<Board> result = boardRepository.findById(100L);
