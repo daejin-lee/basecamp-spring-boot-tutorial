c237e5732507dddc42fe7ce649340d22c06a0f97
daejin-lee <daejin.lee@nhn.com>
2021-01-21 11:11:21 +0900

    Update Board.java

--- board/src/main/java/org/zerock/board/entity/Board.java
+++ board/src/main/java/org/zerock/board/entity/Board.java
@@ -2,21 +2,20 @@ package org.zerock.board.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@ToString
+@ToString(exclude = "writer")
 public class Board extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bno;
     private String title;
     private String content;
+    @ManyToOne
+    private Member writer;
 }
